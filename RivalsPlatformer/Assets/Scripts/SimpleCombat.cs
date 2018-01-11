4622c27baebaf11cd3958b442c571f48241ac24d
Andrew F-F <andrew.francois.fortin@gmail.com>
2018-01-11 17:54:13 -0500

    Hitbox will 100% follow the player
    The hitbox used to get stuck on some objects, now it will follow the player even if it sticks it will go right back to us

--- RivalsPlatformer/Assets/Scripts/SimpleCombat.cs
+++ RivalsPlatformer/Assets/Scripts/SimpleCombat.cs
@@ -1,27 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class SimpleCombat : MonoBehaviour {
+public class SimpleCombat : NetworkBehaviour {
     public Collider2D attackCollider;
     bool isCollided;
-    
-	// Use this for initialization
-	void Start () {
+    const float velocityX=10;
+    const float velocityY = 10;
+
+    // Use this for initialization
+    void Start () {
         isCollided = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown(KeyCode.K) && isCollided)
+        if (Input.GetKeyUp(KeyCode.K) && isCollided)
         {
-            //Attack();
+            Attack();
             Debug.Log("Attack");
-            
+            return;
         }
         
 	}
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == ("Player"))
@@ -53,7 +57,29 @@ public class SimpleCombat : MonoBehaviour {
 
     void Attack()
     {
+        Debug.Log("J'attack foreal");
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Attack to the right");
+            GetComponent<Collider>().gameObject.GetComponent<Rigidbody2D>().velocity= new Vector2(transform.localScale.x*velocityX, 0);
 
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Attack under");
+            Vector2 vect = new Vector2(0,velocityY);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Attack to the left");
+            Vector2 vect = new Vector2(velocityX*-1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("Attack over");
+            Vector2 vect = new Vector2(velocityX * -1, 0);
+        }
     }
 
 
